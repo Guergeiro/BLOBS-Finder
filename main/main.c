ba927a93459e46c2db295cf4e5ebf19bd7205236
Diogo Falcão <diogofalken0@gmail.com>
2019-04-30 18:59:56 +0100

    Minor fixes

--- main/main.c
+++ main/main.c
@@ -81,11 +81,10 @@ int main(int argc, char **argv) {
 	} else if (!strcmp(argv[7], "ALL")) {
 		struct imagem *primeiraImagem = lerFicheiro(argv[2]);
 		calcularBlobs(primeiraImagem, atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
-		mostrarImagens(primeiraImagem);
 		sortImagens(primeiraImagem);
+		determinarDesvioPadrao(primeiraImagem);
 		mostrarImagens(primeiraImagem);
 		mostrarImagemComMaisBlobs(primeiraImagem);
-		determinarDesvioPadrao(primeiraImagem);
 		determinarBlobMenorDesvioPadraoImagem(primeiraImagem);
 		destruirImagem(primeiraImagem);
 	} else if (!strcmp(argv[7], "MEM")) {
